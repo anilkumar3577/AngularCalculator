960184b321c5ebdd02850e6974404752742ecd60
anilkumar3577 <107928396+anilkumar3577@users.noreply.github.com>
2024-02-05 16:18:39 +0530

    Update reactjspipeline.groovy

--- vars/reactjspipeline.groovy
+++ vars/reactjspipeline.groovy
@@ -1,4 +1,33 @@
 def call() {
-   // do some stuff
-   return 'React'
+  pipeline {
+    agent any
+    tools {
+        git 'Default'
+        dockerTool 'docker'
+        maven 'mvn'
+        nodejs 'node'
+    }
+    stages{
+        stage(package){
+            steps {
+               echo 'npm install'
+            }
+        }
+
+        stage(build){
+            steps {
+              sh 'docker build -t share .' 
+            }
+        }
+        stage(push){
+            steps {
+            withCredentials([usernamePassword(credentialsId: 'dock', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+ sh ''' docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+ docker tag share ${DOCKER_USERNAME}/share
+        docker push ${DOCKER_USERNAME}/share '''
+             }
+
+            }
+        }
+    }
 }
